fb19cfd405d5b7c3469590108129a4ada930b244
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-01-17 12:21:20 -0500

    refactor : external file for the connection to MySQL db

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql2/promise");
+require("dotenv").config();
+
+let connection;
+
+async function getConnection() {
+  if (!connection) {
+    connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      port: process.env.DB_PORT,
+    });
+
+    console.log("MySQL Connected");
+  }
+
+  return connection;
+}
+
+module.export = getConnection;
--- server.js
+++ server.js
@@ -1,38 +1,25 @@
 const express = require("express");
-const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const app = express();
-const PORT = 8001;
+const PORT = process.env.PORT || 8001;
 
-async function startServer() {
-  try {
-    const connection = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      port: process.env.DB_PORT,
-    });
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
+// Routes
+app.get("/members", async (req, res) => {
+  const [members] = await connection.query("SELECT * FROM users");
+  res.send({ members });
+});
 
-    app.get("/", (req, res) => {
-      res.json({ message: "ok" });
-    });
+app.get("/", (req, res) => {
+  res.json({ message: "ok" });
+});
 
-    app.get("/members", async (req, res) => {
-      const [members] = await connection.query("SELECT * FROM users");
-      res.send({ members });
-    });
-
-    app.listen(PORT, () => {
-      console.log(`Server listening at http://localhost:${PORT}`);
-    });
-  } catch (err) {
-    console.error("Database connection error:", err);
-  }
-}
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
 
 startServer();
