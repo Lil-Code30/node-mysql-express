74d73d86cf8b16c1771f2b0b0392147ef30fd71a
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-01-17 01:35:44 -0500

    feat : mysql connection (db : user) with the server

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 const express = require("express");
+const mysql = require("mysql2/promise");
 
 const app = express();
-
 const PORT = 8001;
 
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  }),
-);
+async function startServer() {
+  try {
+    const connection = await mysql.createConnection({
+      host: "127.0.0.1",
+      user: "root",
+      password: "azerty237",
+      database: "githut",
+      port: 3306,
+    });
 
-app.get("/", (req, res) => {
-  res.json({ message: "ok" });
-});
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
-});
+    app.get("/", (req, res) => {
+      res.json({ message: "ok" });
+    });
+
+    app.get("/members", async (req, res) => {
+      const [members] = await connection.query("SELECT * FROM users");
+      res.send({ members });
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server listening at http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error("Database connection error:", err);
+  }
+}
+
+startServer();
